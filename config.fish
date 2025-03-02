f91d262381c0487e81a91b27bfa8006f45b80aa7
Cannafox <deuterpl@gmail.com>
2025-03-02 12:58:03 +0100

    update

--- config.fish
+++ config.fish
@@ -17,12 +17,12 @@ and not set -q TMUX
   end
 end
 
-fish_add_path -p $HOME/.cargo/bin/
-fish_add_path -p $HOME/.yarn/bin/
-fish_add_path -p $HOME/node_modules/
+#fish_add_path -p $HOME/.cargo/bin/
+#fish_add_path -p $HOME/.yarn/bin/
+#fish_add_path -p $HOME/node_modules/
 #fish_add_path -p /usr/lib/node_modules/
 #fish_add_path -p /usr/bin/
-fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
-fish_add_path -p $HOME/.local/share/nvim/mason/bin/
+#fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
+#fish_add_path -p $HOME/.local/share/nvim/mason/bin/
 
 fzf_configure_bindings
