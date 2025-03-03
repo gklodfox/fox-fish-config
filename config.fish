0f68c3f707c175f93d7e6a15583f6b3e3b71d8c4
Cannafox <deuterpl@gmail.com>
2025-03-03 06:33:33 +0100

    update

--- config.fish
+++ config.fish
@@ -11,7 +11,7 @@ and not set -q TMUX
   set -gx FZF_DEFAULT_COMMAND "fzf --tmux"
   set -gx FZF_DEFAULT_OPTS "--height 40% --tmux bottom,40% --layout reverse --border --color 'bg+:#ff007b,fg:#141a1f,fg+:#142a2f,hl:#52697a,hl+:#52697a,border:#3d4f5c,info:#ff007b,marker:#a875f0,pointer:#ff007b,prompt:#ff007b,spinner:#ff007b,header:#ff007b'"
   if tmux has-session -t master
-    exec tmux attach-session -t master
+    tmux attach-session -t master
   else
     tmux new-session -s master
   end
@@ -23,6 +23,6 @@ end
 #fish_add_path -p /usr/lib/node_modules/
 #fish_add_path -p /usr/bin/
 #fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
-#fish_add_path -p $HOME/.local/share/nvim/mason/bin/
+fish_add_path -p $HOME/.local/share/nvim/mason/bin/
 
 fzf_configure_bindings
