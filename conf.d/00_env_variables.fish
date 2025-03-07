15c48aaca6c0a093565d1b25b6be9dcee81d94a4
Cannafox <deuterpl@gmail.com>
2025-03-07 02:13:36 +0100

    update

--- conf.d/00_env_variables.fish
+++ conf.d/00_env_variables.fish
@@ -1,13 +1,19 @@
 # Env variables
 # system
+set -gx TERM xterm-ghostty
+set -gx TERM_PROGRAM ghostty
+set -gx TERM_PROGRAM_VERSION 1.1.2-arch
 set -gx COLORTERM truecolor
 set -gx EDITOR nvim
-#set -gx LANG en_US.UTF-8
-#set -gx LC_ALL en_US.UTF-8
-set -gx BROWSER firefox
-set -gx TERM tmux-256color
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx BROWSER qutebrowser
+set -gx VIRTUAL_ENV_DISABLE_PROMPT true
+set -gx DOCKER_BUILDKIT 1
+set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_bind_mode insert
 #set -gx VULKAN_SDK /home/fox/.vulkan/1.3.296.0/x86_64
-set -gx LD_LIBRARY_PATH $VULKAN_SDK/lib
+#set -gx LD_LIBRARY_PATH $VULKAN_SDK/lib
 #set -gx VK_ICD_FILENAMES /usr/share/vulkan/icd.d/nvidia_icd.json
 #set -gx VK_LAYER_PATH $VULKAN_SDK/share/vulkan/explicit_layer.d
 #set -gx VK_ADD_LAYER_PATH $VULKAN_SDK/share/vulkan/explicit_layer.d
@@ -16,6 +22,7 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx AICHAT_PLATFORM openai
 set -gx PYENV_ROOT /home/fox/.pyenv
 set -gx SCRIPT_DIR /home/fox/Scripts
+set -gx FZF_DEFAULT_OPTS_FILE /home/fox/.config/fzf_config.ini
 
 # fish
 #set -g fish_key_bindings fish_vi_key_bindings
--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
+set fish_greeting
+
 # Start X at login
 if status is-login
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
@@ -6,23 +8,25 @@ if status is-login
 end
 
 # Start tmux
-if status is-interactive
-and not set -q TMUX
-  set -gx FZF_DEFAULT_COMMAND "fzf --tmux"
-  set -gx FZF_DEFAULT_OPTS "--height 40% --tmux bottom,40% --layout reverse --border --color 'bg+:#ff007b,fg:#141a1f,fg+:#142a2f,hl:#52697a,hl+:#52697a,border:#3d4f5c,info:#ff007b,marker:#a875f0,pointer:#ff007b,prompt:#ff007b,spinner:#ff007b,header:#ff007b'"
-  if tmux has-session -t master
-    tmux attach-session -t master
-  else
-    tmux new-session -s master
+if status --is-interactive
+  if not tmux has-session -t Main
+    tmux new -s Main
   end
+  if not tmux has-session -t Config
+    tmux new -s Config
+  end
+  if not tmux has-session -t Code
+    tmux new -s Code
+  end
+  tmux attach -t Main
 end
 
-#fish_add_path -p $HOME/.cargo/bin/
+fish_add_path -p $HOME/.cargo/bin/
 #fish_add_path -p $HOME/.yarn/bin/
 #fish_add_path -p $HOME/node_modules/
 #fish_add_path -p /usr/lib/node_modules/
 #fish_add_path -p /usr/bin/
-#fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
+fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
 fish_add_path -p $HOME/.local/share/nvim/mason/bin/
 
-fzf_configure_bindings
+#fzf_configure_bindings
