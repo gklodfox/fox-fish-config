27be98d271864464690c2f2d260a051ec1199afa
gklodfox <gklodkowski@gmail.com>
2025-05-20 00:35:24 +0200

    test

--- config.fish
+++ config.fish
@@ -20,7 +20,7 @@ fish_add_path -p $HOME/.cargo/bin/
 #fish_add_path -p $HOME/node_modules/
 #fish_add_path -p /usr/lib/node_modules/
 #fish_add_path -p /usr/bin/
-fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
+#fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
 fish_add_path -p $HOME/.local/share/nvim/mason/bin/
 #fzf --fish | source
 
