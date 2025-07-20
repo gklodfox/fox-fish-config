695827c5a924b5fef6180fe7df3ce6f856bdb977
gklodfox <gklodkowski@gmail.com>
2025-07-21 01:26:21 +0200

    sync

--- .config/fish/conf.d/02_fish_settings.fish
+++ .config/fish/conf.d/02_fish_settings.fish
@@ -1,5 +1,4 @@
 set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_tmux_autostart true
 set -g fish_bind_mode insert
-
 fish_add_path --prepend --path /home/fox/.local/share/gem/ruby/3.4.0/bin
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,3 @@
-set fish_greeting
-
 status is-interactive; and begin
-    set fish_tmux_autostart true
+    # Interactive mode
 end
-
-# fish_add_path -p $HOME/.cargo/bin/
-#fish_add_path -p $HOME/.yarn/bin/
-#fish_add_path -p $HOME/node_modules/
-#fish_add_path -p /usr/lib/node_modules/
-#fish_add_path -p /usr/bin/
-#fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
-# fish_add_path -p $HOME/.local/share/gem/ruby/3.3.0/bin/
-# fish_add_path -p $HOME/.local/share/nvim/mason/bin/
-#fzf --fish | source
-
-#fzf_configure_bindings
